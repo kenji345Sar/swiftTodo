e322b16b980d7b3a8c99d1c30dc958eac40971e8
sawa <you@example.com>
2024-02-11 16:33:18 +0900

    子コンポーネント化

--- TodoApp/ContentView.swift
+++ TodoApp/ContentView.swift
@@ -7,63 +7,48 @@
 
 import SwiftUI
 
-
-struct Task: Identifiable,Equatable {
+struct Task: Identifiable, Equatable {
     let id: Int
     var title: String
     var checked: Bool
-    
-    init(id: Int, title: String, checked: Bool) {
-        self.id = id
-        self.title = title
-        self.checked = checked
-    }
 }
-
 
 struct ContentView: View {
-    
     @State private var tasks = [
-        Task(id:1,title:"散歩",checked: true),
-        Task(id:2,title:"料理",checked:false),
-        Task(id:3,title:"筋トレ",checked: true)
+        Task(id: 1, title: "散歩", checked: true),
+        Task(id: 2, title: "料理", checked: false),
+        Task(id: 3, title: "筋トレ", checked: true)
     ]
     
-    
     var body: some View {
-            List{
-                ForEach(tasks.indices, id: \.self) { index in
-                    Button(action: {
-                        // 直接tasksの要素を変更
-                        tasks[index].checked.toggle()
-                    }) {
-                        ListRow(task: tasks[index].title, isCheck: tasks[index].checked)
-                    }
-                }
-            }
- 
-    }
-}
-
-struct ListRow: View {
-    let task: String
-    var isCheck: Bool
-
-    var body: some View {
-        HStack {
-            if isCheck {
-                Text("☑︎")
-                Text(task)
-                    .strikethrough()
-                    .fontWeight(.ultraLight)
-            } else {
-                Text("□")
-                Text(task)
+        List {
+            ForEach($tasks) { $task in
+                ListRow(taskTitle: $task.title, isChecked: $task.checked)
             }
         }
     }
 }
 
+struct ListRow: View {
+    @Binding var taskTitle: String
+    @Binding var isChecked: Bool
+    
+    var body: some View {
+        Toggle(isOn: $isChecked) {
+            if isChecked {
+                Text(taskTitle)
+                    .strikethrough()
+                    .fontWeight(.ultraLight)
+            } else {
+                Text(taskTitle)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
 
 //struct ContentView_Previews: PreviewProvider {
 //    static var previews: some View {
@@ -71,6 +56,4 @@ struct ListRow: View {
 //    }
 //}
 
-#Preview {
-    ContentView()
-}
+
